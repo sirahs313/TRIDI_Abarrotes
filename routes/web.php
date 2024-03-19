d04061cd8ff8367229f0bb6df8ea0c9080a56426
Erick Hernández Suarez <siraksua@gmail.com>
2024-03-19 08:23:27 -0600

    Bootstrap UI

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/', function () {
 });
 
 Route::get('principal', [UserController::class, 'home']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
