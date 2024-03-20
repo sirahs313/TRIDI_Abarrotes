a67f72f17812f0df78c03843b258c33fcc8fd550
arturozoberzau <ramirezgarturo@micorreo.upp.edu.mx>
2024-03-19 22:55:48 -0600

    Juan jeus Cree el controlador y puse sus  rutas

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ Route::get('principal', [UserController::class, 'home']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// formulario de nueva venta
+Route::get('/ventas/create', [VentaController::class, 'create'])->name('ventas.create');
+// almacenamiento de una nueva venta
+Route::post('/ventas', [VentaController::class, 'store'])->name('ventas.store');
