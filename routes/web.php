253817f63483f607e62a8b36a4bbbd4d9be88229
Jisusz <juanjesus.fm08@gmail.com>
2024-03-24 21:29:38 -0600

    Agregue la vista de ventas

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\VentaController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -12,7 +12,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// formulario de nueva venta
+
 Route::get('/ventas/create', [VentaController::class, 'create'])->name('ventas.create');
-// almacenamiento de una nueva venta
 Route::post('/ventas', [VentaController::class, 'store'])->name('ventas.store');
