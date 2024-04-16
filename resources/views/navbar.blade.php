8ecb72865d5f8898d921703c2993a7561af0f91e
AxDan21 <axeldan21@outlook.com>
2024-04-15 23:46:37 -0600

    Añadir nombre de usuario y botón de logout al navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,3 +1,5 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <div class="container-fluid">
         <ul class="navbar-nav">
@@ -11,5 +13,18 @@
                 <a class="nav-link" href="{{ route('producto.index') }}">Productos</a>
             </li>
         </ul>
+        @if (Auth::check())
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <span class="nav-link">Bienvenido, {{ Auth::user()->name }}</span>
+                </li>
+                <li class="nav-item">
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-link nav-link">Logout</button>
+                    </form>
+                </li>
+            </ul>
+        @endif
     </div>
 </nav>
